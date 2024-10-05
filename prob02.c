7d7eb576dc74ffa0bdb7eb0c141196fbe54ba95b
Ivan-Soncco <isoncco@unsa.edu.pe>
2024-10-04 23:15:40 -0500

    se intento otra manera de usar el bucle, uniendo un cuadrado negro y blanco con la funcion join 4 veces, no funciono solo salio una vez

--- prob02.c
+++ prob02.c
@@ -2,10 +2,11 @@
 #include "figures.h"
 
 void display(){
-  char** whiteLine=repeatH(whiteSquare, 8);
-  for(int i=0;i<8;i+=2){
-    whiteLine[i]=reverse(whiteLine[i]);
+  char** line;
+
+  for(int i=0;i<4;i++){
+    line=join(reverse(whiteSquare),whiteSquare);
   }
-  interpreter(whiteLine);
+  interpreter(line);
 }
 	
