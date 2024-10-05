e439ba286c52f939c73372127f7d8f89fe555a30
JoseJesusH <kills.rampage@gmail.com>
2024-10-04 23:50:19 -0500

    se intento imprimir 4 columnas de los cuadrados gris y blanco intercalado, pero con el camino seguido generamos 4 columnas con el mismo patron

--- prob03.c
+++ prob03.c
@@ -1,4 +1,16 @@
-#include
+#include "chess.h"
+#include "figures.h"
+#include <stdlib.h>
 
-
-void display()
+void display(){
+	char** white = whiteSquare;
+	char** black = reverse(white);
+	char** pair = join(black, white);
+	char** line = repeatH(pair, 4);
+	char** square = repeatV(line, 4);
+	interpreter(square);
+	free(square);
+	free(line);
+	free(pair);
+	free(black); 
+}
