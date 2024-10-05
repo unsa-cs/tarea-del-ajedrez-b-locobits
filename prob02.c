d39344c2448d013178a4813328ca5e10563ccddd
Maurpz <jhualpal@unsa.edu.pe>
2024-10-04 23:33:43 -0500

    Para crear la linea al igual que en el ejercicio 1 se utilizo la funcion repeat(), al haber usado la funcion antes solo tuvimos que pasarle pair

--- prob02.c
+++ prob02.c
@@ -5,7 +5,9 @@ void display(){
   char** blanco = whiteSquare;
   char** negro = reverse(blanco);
   char** pair = join(negro, blanco);
-  interpreter(pair);
+  char** row = repeatH(pair, 4);
+  interpreter(row);
+  free(row);
   free(pair);
   free(negro);
 }
