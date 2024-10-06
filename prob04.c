c5a1af63557b4d4a3118b145053728c50f193daa
Ivan-Soncco <isoncco@unsa.edu.pe>
2024-10-05 22:34:02 -0500

    se probo una forma de crear una linea de blancos y grises a traves de un bucle, se esta evaluando si seguir esta idea o volver a la implementacion anterior

--- prob04.c
+++ prob04.c
@@ -2,9 +2,13 @@
 #include "figures.h"
 #include <stdlib.h>
 void display(){
-	char** figure = knight;
-	char** square = whiteSquare;
-	char** prob = superImpose(figure,square);
-	interpreter(prob);
-	free(prob);
+  /*char** figure = knight;
+  char** square = whiteSquare;
+  char** prob = superImpose(figure,square);*/
+  char** linea=reverse(whiteSquare);
+  for(int i=0;i<7;i++){
+    linea = i%2==0 ? join(linea,whiteSquare) : join(linea,reverse(whiteSquare));
+  }
+  interpreter(linea);
+  free(linea);
 }
