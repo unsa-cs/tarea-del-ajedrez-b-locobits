db44200051155b06671e562cbe5b78095f9a40b3
Ivan-Soncco <isoncco@unsa.edu.pe>
2024-10-05 22:43:08 -0500

    se prueba superImpose para añadir 1 torre al primer cuadrado de la linea,se evalua la idea de agregar todas las figuras en el bucle

--- prob04.c
+++ prob04.c
@@ -5,10 +5,12 @@ void display(){
   /*char** figure = knight;
   char** square = whiteSquare;
   char** prob = superImpose(figure,square);*/
-  char** linea=reverse(whiteSquare);
+  char** arregloF[]={rook, knight, bishop, queen, king, bishop, knight, rook};
+  char** linea=superImpose(arregloF[0],reverse(whiteSquare));
   for(int i=0;i<7;i++){
     linea = i%2==0 ? join(linea,whiteSquare) : join(linea,reverse(whiteSquare));
   }
+
   interpreter(linea);
   free(linea);
 }
