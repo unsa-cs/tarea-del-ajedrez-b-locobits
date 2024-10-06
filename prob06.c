ee88497cf4fdc65f04f895e7c68930c606507585
JoseJesusH <kills.rampage@gmail.com>
2024-10-05 23:46:28 -0500

    Se intento generar la nueva figura de la linea con las figuras con los peones utilizando la funcion up() lo cual nos dio un error segmentation fault (core dumped), se evaluara cual es la posible solucion

---
+++ prob06.c
@@ -0,0 +1,24 @@
+#include "figures.h"
+#include "chess.h"
+#include <stdlib.h>
+
+void display () {
+  char** arregloF[]={rook, knight, bishop, queen, king, bishop, knight, rook};
+  char** linea=superImpose(reverse(arregloF[0]),reverse(whiteSquare));
+	char** lineap= superImpose(reverse(pawn), whiteSquare);  
+	for(int i=0;i<7;i++){
+  	if(i%2==0){
+			linea=join(linea, superImpose(reverse(arregloF[i+1]), whiteSquare));
+			lineap=join(lineap, superImpose(reverse(pawn), reverse(whiteSquare)));
+		}
+		else{
+			linea= join(linea, superImpose(reverse(arregloF[i+1]), reverse(whiteSquare)));
+			lineap= join(linea, superImpose(reverse(pawn), whiteSquare));
+		}
+	linea=up(linea, lineap);
+	interpreter(linea);
+	free(linea);
+	free(lineap);
+	} 
+} 
+
